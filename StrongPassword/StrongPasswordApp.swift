39cff107bf747f4b8c40b29b3e84aa79848228bc
githubuiux <uiuxista@gmail.com>
2023-09-12 15:35:53 +0300

    Initial Commit

---
+++ StrongPassword/StrongPasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongPasswordApp.swift
+//  StrongPassword
+//
+//  Created by Levent on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct StrongPasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
